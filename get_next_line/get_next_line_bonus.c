ab3372247ba0562b8502ad80c00f4cf83fd3a366
nicholasbear <50478347+nicholasbear@users.noreply.github.com>
2022-09-25 17:47:03 +0900

    Add files via upload

---
+++ get_next_line/get_next_line_bonus.c
@@ -0,0 +1,87 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wookim2 <wookim2@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/22 15:57:17 by wookim2           #+#    #+#             */
+/*   Updated: 2022/08/22 16:09:48 by wookim2          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+int	find_nl(char *s)
+{
+	size_t	i;
+
+	if (!s)
+		return (-2);
+	i = 0;
+	while (s[i] && s[i] != '\n')
+		i++;
+	if (s[i])
+		return (i);
+	return (-1);
+}
+
+char	*nl_inline(char **back_up, int place)
+{
+	char	*line;
+	char	*new_back_up;
+
+	new_back_up = ft_strdup(*back_up + place + 1);
+	line = (char *)malloc(sizeof(char) * place + 2);
+	if (!line)
+	{
+		free(new_back_up);
+		return (NULL);
+	}
+	ft_strlcpy(line, *back_up, place + 2);
+	free(*back_up);
+	*back_up = new_back_up;
+	return (&line[0]);
+}
+
+char	*last(char **back_up)
+{
+	char	*line;
+
+	line = ft_strdup(*back_up);
+	if (!line || ft_strlen(line) == 0)
+	{
+		free(*back_up);
+		free(line);
+		*back_up = NULL;
+		return (NULL);
+	}
+	free(*back_up);
+	*back_up = NULL;
+	return (line);
+}
+
+char	*get_next_line(int fd)
+{
+	char		buf[BUFFER_SIZE + 1];
+	static char	*fd_table[OPEN_MAX] = {0, };
+	int			place;
+	int			read_val;
+
+	if (fd < 0 || OPEN_MAX <= fd || BUFFER_SIZE <= 0)
+		return (NULL);
+	while (1)
+	{
+		place = find_nl(fd_table[fd]);
+		if (place >= 0)
+			return (nl_inline(&fd_table[fd], place));
+		read_val = read(fd, buf, BUFFER_SIZE);
+		if (read_val <= 0)
+			break ;
+		buf[read_val] = '\0';
+		fd_table[fd] = ft_strjoin(fd_table[fd], &buf[0]);
+		if (!fd_table[fd])
+			return (NULL);
+	}
+	return (last(&fd_table[fd]));
+}
---
+++ get_next_line/get_next_line_utils_bonus.c
@@ -0,0 +1,82 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wookim2 <wookim2@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/22 15:57:51 by wookim2           #+#    #+#             */
+/*   Updated: 2022/08/22 16:40:52 by wookim2          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+size_t	ft_strlen(char *str)
+{
+	int	temp;
+
+	temp = 0;
+	while (str[temp])
+	{
+		temp++;
+	}
+	return (temp);
+}
+
+char	*ft_strdup(char *src)
+{
+	int		length;
+	char	*copy;
+	int		i;
+
+	if (!src)
+		return (NULL);
+	length = ft_strlen(src);
+	copy = (char *)malloc(sizeof(char) * (length + 1));
+	if (!(copy))
+		return (NULL);
+	i = 0;
+	while (src[i])
+	{
+		copy[i] = src[i];
+		i++;
+	}
+	copy[i] = '\0';
+	return (copy);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char	*temp;
+	size_t	s1_len;
+	size_t	s2_len;
+
+	if (!s1)
+		return (ft_strdup(s2));
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	temp = (char *)malloc(sizeof(char) * (s1_len + s2_len + 1));
+	if (!temp)
+	{
+		free(s1);
+		return (NULL);
+	}
+	ft_strlcpy(temp, s1, s1_len + 1);
+	ft_strlcpy(temp + s1_len, s2, s2_len + 1);
+	free(s1);
+	return (temp);
+}
+
+void	ft_strlcpy(char *dest, char *src, size_t size)
+{
+	size_t	temp;
+
+	temp = 0;
+	while (temp < size - 1 && src[temp])
+	{
+		dest[temp] = src[temp];
+		temp++;
+	}
+	dest[temp] = '\0';
+}
